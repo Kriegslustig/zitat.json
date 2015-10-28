36929d39c0df98441e744153366b8e6e45918e8b
Kriegslustig <github@ls7.ch>
2015-10-28 11:02:37 +0100

    Comment all the thingsgrep -r /* ./*

--- client/helpers/quoteList.js
+++ client/helpers/quoteList.js
@@ -1,5 +1,7 @@
+// The current quote is saved here
 Session.setDefault('currentQuote', 1)
 
+// This gets and inserts a new quote
 Template.quoteList.helpers({
   currentQuote: function () {
     var currentQuote = Quotes.find().fetch()[Session.get('currentQuote')]
--- client/main.js
+++ client/main.js
@@ -1,8 +1,11 @@
 Meteor.startup(function () {
+  // Get all quotes from the server
   Meteor.subscribe('allQuotes', function () {
 
+    // Render the quote template
     Blaze.render(Template.quoteList, document.body)
 
+    // This sets a new quote
     setInterval(function () {
       Session.set('currentQuote', Math.round(Math.random() * Quotes.find().count() - 1))
     }, 4000)
